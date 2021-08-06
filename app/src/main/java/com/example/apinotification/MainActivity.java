aa3569dd6b5283cfcb262297e2742300ce5621ca
idanishkalam <idanishkalam@gmail.com>
2021-08-07 01:56:10 +0530

    first commit

---
+++ app/src/main/java/com/example/apinotification/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.apinotification;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if(task.isSuccessful())
+                        {
+                            String token=task.getResult();
+                            Log.d("token",token);
+                        }
+                    }
+                });
+    }
+}
